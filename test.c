3500075fb0e2e01da509cb65a7a8fe32464c410f
Michal Vokac <vokac.m@gmail.com>
2011-01-24 15:15:04 +0100

    Some documentation added

--- test.c
+++ test.c
@@ -1,3 +1,6 @@
+/*
+ * AVR microcontroler testing program.
+*/
 #include <avr/io.h>
 #define F_CPU 16000000  	/* 16MHz - takt MCU*/
 #include <util/delay.h>
@@ -8,8 +11,9 @@ int main(void){
 DDRB = 0xff;
 
 while (1) {
+	// red LED blinking
         PORTB = 2; // led ON
-        _delay_ms(500);
+        _delay_ms(50);
         PORTB = 0;	// led OFF
         _delay_ms(500);
 	}
